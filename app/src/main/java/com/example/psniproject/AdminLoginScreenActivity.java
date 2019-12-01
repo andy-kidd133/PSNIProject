72d8abf9c861cd47e6c2f40a2890b67c788fd930
Andrew Kidd <andrew-kidd@outlook.com>
2019-12-01 18:43:51 +0000

    Changed from constraint to relativeLayouts, aligned editTexts and buttons

--- app/src/main/java/com/example/psniproject/AdminLoginScreenActivity.java
+++ app/src/main/java/com/example/psniproject/AdminLoginScreenActivity.java
@@ -1,14 +1,7 @@
 package com.example.psniproject;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class AdminLoginScreenActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/psniproject/LoginScreenActivity.java
+++ app/src/main/java/com/example/psniproject/LoginScreenActivity.java
@@ -4,13 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -27,10 +21,11 @@ public class LoginScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_login_screen);
 
+        setContentView(R.layout.activity_login_screen);
         adminButton = findViewById(R.id.adminBtn);
         loginButton = findViewById(R.id.loginBtn);
         username = findViewById(R.id.etUsername);
