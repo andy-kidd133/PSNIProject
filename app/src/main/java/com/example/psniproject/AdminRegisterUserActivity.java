763e58d02e05c054b9216d947fbed0761f8ba867
Andrew Kidd <andrew-kidd@outlook.com>
2019-12-02 23:00:12 +0000

    Linked FirebaseDB for user authentication

--- app/src/main/java/com/example/psniproject/AdminRegisterUserActivity.java
+++ app/src/main/java/com/example/psniproject/AdminRegisterUserActivity.java
@@ -7,17 +7,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminRegisterUserActivity extends AppCompatActivity {
 
     private EditText firstName, surname, userName, email, password;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_register_user);
         setupUIViews();
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         Button backButton = findViewById(R.id.backBtn);
         Button regButton = findViewById(R.id.regBtn);
@@ -27,7 +36,22 @@ public class AdminRegisterUserActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(validateRegistration()) {
-                    //register to Firebase
+                    //Upload data to "VictimSupportDB"
+                    String user_email = email.getText().toString().trim();
+                    String user_password = password.getText().toString().trim();
+
+                    firebaseAuth.createUserWithEmailAndPassword(user_email, user_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (task.isSuccessful()) {
+                                Toast.makeText(AdminRegisterUserActivity.this, "Registration Successful.", Toast.LENGTH_SHORT).show();
+                            }else {
+                                Toast.makeText(AdminRegisterUserActivity.this, "Registration Failed.", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+                    });
                 }
             }
         });
--- app/src/main/java/com/example/psniproject/LoginScreenActivity.java
+++ app/src/main/java/com/example/psniproject/LoginScreenActivity.java
@@ -1,22 +1,33 @@
 package com.example.psniproject;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginScreenActivity extends AppCompatActivity {
 
-    Button adminButton;
-    Button loginButton;
-    EditText username;
-    EditText password;
+    private Button loginButton;
+    private EditText username, password;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +37,52 @@ public class LoginScreenActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_login_screen);
-        adminButton = findViewById(R.id.adminBtn);
+        Button adminButton = findViewById(R.id.adminBtn);
         loginButton = findViewById(R.id.loginBtn);
         username = findViewById(R.id.etUsername);
         password = findViewById(R.id.etPassword);
-        username.getBackground().setColorFilter(Color.WHITE, PorterDuff.Mode.SRC_ATOP);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
 
         adminButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginScreenActivity.this, AdminLoginScreenActivity.class));
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateCredentials(username.getText().toString(), password.getText().toString());
+            }
+        });
+    }
+
+    private void validateCredentials(String userEmail, String userPassword) {
+
+        progressDialog.setMessage("Signing In..");
+        progressDialog.show();
+
+        firebaseAuth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginScreenActivity.this, "Login Successful.", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(LoginScreenActivity.this, MainActivity.class));
+                }else {
+                    counter--;
+                    Toast.makeText(LoginScreenActivity.this, "You have: " + String.valueOf(counter) + " attempts remaining", Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                    Toast.makeText(LoginScreenActivity.this, "Username or Password incorrect.", Toast.LENGTH_SHORT).show();
+                    if(counter==0) {
+                        loginButton.setEnabled(false);
+                    }
+                }
             }
         });
     }
